eee2dc7ec6f23ad084a8a849a49838206a3cafaa
Yen-Linh <dang.yenlinh@yahoo.co.uk>
2021-04-03 15:34:33 +0100

    edited the .read_csv command to accommodate for no headings in the CSV file and added our own headings to make pulling data later on easier

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 print("Hello World!")
 
 # low_memory parameter to allow for mixed data types on a column in the dataframe
-training_set = pd.read_csv("comp3208-train.csv", low_memory=False)
+training_set = pd.read_csv("comp3208-train.csv", low_memory=False, header=None, names=["user", "itemID", "rating",
+                                                                                       "timestamp"])
 
 print(training_set)
