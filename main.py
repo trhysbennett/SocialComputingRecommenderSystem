9d17e4fc428d7cae4a58e1044f98073e0b495362
Yen-Linh <dang.yenlinh@yahoo.co.uk>
2021-04-03 16:33:30 +0100

    shuffled the training set so we can do a 80-20 split on the training data, so we can use the validation set as the initial test set to have the actual ratings to compare when we do our test predictions to adjust our algorithm to make our f1 score higher

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import numpy as np
 print("Hello World!")
 
 # low_memory parameter to allow for mixed data types on a column in the dataframe
-training_set = pd.read_csv("comp3208-train.csv", low_memory=False, header=None, names=["user", "itemID", "rating",
-                                                                                       "timestamp"])
+initial_training_set = pd.read_csv("comp3208-train.csv", low_memory=False, header=None, names=["user", "itemID",
+                                                                                               "rating", "timestamp"])
+print(initial_training_set)
 
-print(training_set)
+# shuffle the rows in the data frame to make it unique with each run
+initial_training_set = initial_training_set.sample(frac=1)
+print(initial_training_set)
+
+# splitting the training set into a training set (80%) and a validation set (20%) to use
+training_set = initial_training_set.iloc[:80, :]
+validation_set = initial_training_set.iloc[80:, :]
+
+test_set = pd.read_csv("comp3208-test.csv", low_memory=False, header=None, names=["user", "itemID", "timestamp"])
