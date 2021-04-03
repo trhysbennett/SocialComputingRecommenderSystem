73e13fde10bf0a8a83e20d5fbab14e76373f02e2
Yen-Linh <dang.yenlinh@yahoo.co.uk>
2021-04-03 15:26:51 +0100

    added import statement for pandas and  read in the training set. Also added numpy import.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+
 print("Hello World!")
+
+# low_memory parameter to allow for mixed data types on a column in the dataframe
+training_set = pd.read_csv("comp3208-train.csv", low_memory=False)
+
+print(training_set)
