9edfb47f41761bc7a7c9f84ec3b8dc0cc839efb9
Yen-Linh <dang.yenlinh@yahoo.co.uk>
2021-04-13 00:22:14 +0100

    fixed sim(), calcualte_sim(), get_neighbours() and pred()
    pandas dataframe takes too long to run through and calcualte the sim of each user, therefore we should change to a sqlite3 database

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ initial_training_set = pd.read_csv("comp3208-train.csv", low_memory=False, heade
 # shuffle the rows in the data frame to make it unique with each run
 initial_training_set = initial_training_set.sample(frac=1)
 
-initial_training_set.apply(lambda rating: pd.to_numeric(rating, errors='coerce')).dropna()
+initial_training_set = initial_training_set.apply(pd.to_numeric, errors='coerce')
+initial_training_set['rating'] = initial_training_set['rating'].astype(float)
 initial_training_set = initial_training_set[(initial_training_set > 0).all(axis=1)]
 
 # splitting the training set into a training set (80%) and a validation set (20%) to use
@@ -19,8 +20,6 @@ validation_set = initial_training_set.iloc[80:, :]
 
 test_set = pd.read_csv("comp3208-test.csv", low_memory=False, header=None, names=["user", "itemID", "timestamp"])
 
-print(initial_training_set['user'])
-
 
 # u1 and u2 are lists of ratings for the same items from user1 and user2
 # Returns the similarity measure for user1 and user2
@@ -28,22 +27,20 @@ def sim(u1, u2):
     mean_u1 = sum(u1) / len(u1)
     mean_u2 = sum(u2) / len(u2)
 
-    sum_u1 = 0
-    sum_u2 = 0
+    sum_us = 0
 
     sum_sqr_u1 = 0
     sum_sqr_u2 = 0
 
-    for i in u1:
-        sum_u1 += i - mean_u1
-        sum_sqr_u1 += (i - mean_u1) ^ 2
+    for i in range(len(u1)):
+        sum_us += (u1[i] - mean_u1) * (u2[i] - mean_u2)
+        sum_sqr_u1 += (u1[i] - mean_u1) ** 2
+        sum_sqr_u2 += (u2[i] - mean_u2) ** 2
 
-    for i in u2:
-        sum_u2 += i - mean_u2
-        sum_sqr_u2 += (i - mean_u2) ^ 2
-
-    print((sum_u1 * sum_u2) / (math.sqrt(sum_sqr_u1) * math.sqrt(sum_sqr_u2)))
-    return (sum_u1 * sum_u2) / (math.sqrt(sum_sqr_u1) * math.sqrt(sum_sqr_u2))
+    if (math.sqrt(sum_sqr_u1) == 0.0) | (math.sqrt(sum_sqr_u2) == 0.0):
+        return 0
+    else:
+        return sum_us / (math.sqrt(sum_sqr_u1) * math.sqrt(sum_sqr_u2))
 
 
 # Given two users user1 and user2 create a list of ratings for items that both user1 and user2 have reviewed
@@ -55,10 +52,10 @@ def calculate_sim(user1, user2):
 
     for item in u1_items:
         try:
-            u1.append(get_single_rating(user1, item))
             u2.append(get_single_rating(user2, item))
-        except:
-            print("Item not rated by user2")
+            u1.append(get_single_rating(user1, item))
+        except Exception:
+            pass
 
     return sim(u1, u2)
 
@@ -82,7 +79,8 @@ def get_single_rating(user, item):
 
 
 def get_neighbours(user, item):
-    neighbours = {}
+    neighbours = list(initial_training_set.loc[(initial_training_set['user'] != user) &
+                                               (initial_training_set['itemID'] == item), ['user']].iloc[:, 0])
 
     return neighbours
 
@@ -109,4 +107,4 @@ def pred(user, item):
     return mean_user_rating + (numerator / denominator)
 
 
-calculate_sim(1, 2)
+print(pred(1, 46886))
